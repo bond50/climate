04551b1d2efa26ea73861df9f5fc4645297ad034
bobd50 <galavu10@gmail.com>
2025-02-23 20:59:08 +0300

    first

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+        {
+            name: "my-app",
+            script: "npm",
+            args: "run start",
+            env: {
+                NODE_ENV: "production"
+            }
+        }
+    ]
+};
+
